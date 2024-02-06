053be542c686e4e71cb97d5d8f4dc37e86773b67
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-02-06 13:47:47 +0530

    test

--- set01/problem10.c
+++ set01/problem10.c
@@ -2,7 +2,7 @@
 // For example, `Hello` should be equal to `Hello`, but not equal to `hello`.
 // `Hello` will be lesser than `Hellw` (alphabetical order).
 #include <stdio.h>
-#include <string.h>
+
 void input_two_strings(char *string1, char *string2);
 int stringcompare(char *string1, char *string2);
 void output(char *string1, char *string2, int result);
@@ -21,9 +21,9 @@ int main()
 void input_two_strings(char *string1, char *string2)
 {
   printf("Enter the first string\n");
-  scanf("%s[^\n]", string1);
+  scanf("%s", string1);
   printf("Enter the second string\n");
-  scanf("%s[^\n]", string2);
+  scanf("%s", string2);
 }
 
 int stringcompare(char *string1, char *string2)
@@ -35,15 +35,19 @@ int stringcompare(char *string1, char *string2)
     return 0;
   if(string1[i]>string2[i])
     return 1;
-  else return 2;
+  else 
+  {
+    return 2;
+  }
+
 }
 
 void output(char *string1, char *string2, int result)
 {
   if(result==0)
-    printf("%s is equal to %s\n", string1, string2);
+    printf("%s is equal to %s", string1, string2);
   else if(result==1)
-    printf("%s is greater than %s\n", string1, string2);
+    printf("%s is greater than %s", string1, string2);
   else if(result==2)
-    printf("%s is greater than %s\n", string2, string1);
+    printf("%s is greater than %s", string2, string1);
 }
