e697e3698725fea4af7beda3220c2504c4a9a258
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-02-11 13:59:56 +0530

    test

---
+++ set03/problem04.c
@@ -0,0 +1,24 @@
+// Write a program to find nth number in fibonacci sequence.
+int input();
+int find_fibo(int n);
+void output(int n, int fibo);
+int main()
+{
+    int n,fibo;
+    n = input();
+    fibo = find_fibo(n);
+    output(n, fibo);
+    return 0;
+}
+int input()
+{
+
+}
+int find_fibo(int n)
+{
+
+}
+void output(int n, int fibo)
+{
+
+}
