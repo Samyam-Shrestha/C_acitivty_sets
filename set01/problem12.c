5424222c1f5e65b3a1cb7ed2752130c4c2c9253d
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-02-13 10:34:56 +0530

    test

--- set01/problem12.c
+++ set01/problem12.c
@@ -64,5 +64,10 @@ Complex add_n_complex(int n, Complex c[n])
 }
 void output(int n, Complex c[n], Complex result)
 {
-
+    printf("Sum of %d complex numbers:\n", n);
+    for (int i = 0; i < n; i++) 
+    {
+        printf("%.2f + %.2fi\n", c[i].real, c[i].imaginary);
+    }
+    printf("= %.2f + %.2fi\n", result.real, result.imaginary);  
 }
