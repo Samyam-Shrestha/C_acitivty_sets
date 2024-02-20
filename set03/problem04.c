d51c19fd02258ede2d63ec57e938c2c5984c808e
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-02-20 13:46:48 +0530

    test

--- set03/problem04.c
+++ set03/problem04.c
@@ -20,9 +20,16 @@ int input()
 }
 int find_fibo(int n)
 {
-
+    int a[n];
+    for(int i=2;i<=n;i++)
+    {
+        a[0]=0,a[1]=1;
+        
+        a[i]=a[i-1]+a[i-2];
+    }
+    return a[n];
 }
 void output(int n, int fibo)
 {
-
+    printf("fibo(%d) = %d \n",n,fibo);
 }
