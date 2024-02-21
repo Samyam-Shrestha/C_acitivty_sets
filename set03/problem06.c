8286a7e4a7fac35da28e565fa213bb2af88ac395
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-02-21 21:25:20 +0530

    test

---
+++ set03/problem06.c
@@ -0,0 +1,5 @@
+// Write a program to find the index of a substring of a string
+#include <stdio.h>
+void input_string(char* a, char* b);
+int sub_str_index(char* string, char* substring);
+void output(char *string, char *substring, int index);
