e15ecf8305b37ca9f6906f02845465a0bbcbbf33
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-02-22 12:08:34 +0530

    test

--- set03/problem06.c
+++ set03/problem06.c
@@ -3,3 +3,19 @@
 void input_string(char* a, char* b);
 int sub_str_index(char* string, char* substring);
 void output(char *string, char *substring, int index);
+int main()
+{
+
+}
+void input_string(char* a, char* b)
+{
+
+}
+int sub_str_index(char* string, char* substring)
+{
+
+}
+void output(char *string, char *substring, int index)
+{
+    
+}
