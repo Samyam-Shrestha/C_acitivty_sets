54db0af61a3321cf2de66298aabb3058bc463935
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-02-24 13:50:32 +0530

    test

--- set03/problem01.c
+++ set03/problem01.c
@@ -1,5 +1,7 @@
 // Write a program to find the distance between 2 points
 #include <stdio.h>
+#include <math.h>
+
 void input(float *x1, float *y1, float *x2, float *y2);
 float find_distance(float x1, float y1, float x2, float y2);
 void output(float x1, float y1, float x2, float y2, float distance);
@@ -25,9 +27,10 @@ void input(float *x1, float *y1, float *x2, float *y2)
 float find_distance(float x1, float y1, float x2, float y2)
 {
     float distance;
-    
+    distance=sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+    return distance;
 }
 void output(float x1, float y1, float x2, float y2, float distance)
 {
-
+    printf("The distance between point (%f,%f) and (%f,%f) is %f",x1,x2,y1,y2,distance);
 }
