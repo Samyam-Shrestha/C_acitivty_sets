28308795f13a8cf0d3ac0c6645d91a580b3892ca
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-02-25 13:48:03 +0530

    test

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,32 +1,35 @@
-// Write a C program to find the [square root] using Babylonian Method
-#include <stdio.h>
-
+//9. Write a C program to find the [square root](https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Babylonian_method) of a number.
+#include<stdio.h>
 float input();
 float square_root(float n);
 void output(float n, float sqrroot);
 
 int main()
 {
-    float inp, sqrrt;
-    inp=input();
-    sqrrt=square_root(inp);
-    output(inp,sqrrt);
+    float n,sqrt;
+    n = input();
+    sqrt = square_root(n);
+    output(n,sqrt);
 }
-
-float input() 
+float input()
 {
-    float inp;
-    printf("enter the number:");
-    scanf("%f", &inp);
-    return inp;
+    float n;
+    printf("Enter the number: ");
+    scanf("%f",&n);
+    return n;
 }
-
-float square_root(float n) 
+float square_root(float n)
 {
-  
-}
+    float x = n, y = 1.0, epsilion = 0.00001;
+    while (x - y > epsilion)
+    {
+        x = (x+y)/2;
+        y = n/x;
+    }
 
+    return x;
+}
 void output(float n, float sqrroot)
 {
-  printf("the squareroot of %f is %f", n, sqrroot);
+    printf("The square root of %f is : %f",n,sqrroot);
 }
