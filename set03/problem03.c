b2239cf77ea5a02eccf213e62474ae9ef6d92073
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-02-28 13:36:26 +0530

    test

---
+++ set03/problem03.c
@@ -0,0 +1,24 @@
+// Write a program to check if the given number is prime
+int input_number();
+int is_prime(int n);
+void output(int n, int result);
+int main()
+{
+    int n,result;
+    n = input_number();
+    result = is_prime(n);
+    output(n, result);
+    return 0;
+}
+int input_number()
+{
+
+}
+int is_prime(int n)
+{
+
+}
+void output(int n, int result)
+{
+
+}
