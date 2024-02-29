c0aac66a9301bb4247f43f934b39de758f7382e3
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-02-29 14:38:57 +0530

    test

--- set03/problem03.c
+++ set03/problem03.c
@@ -19,7 +19,19 @@ int input_number()
 }
 int is_prime(int n)
 {
-
+     int result=0;
+    for(int i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            result++;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return result;
 }
 void output(int n, int result)
 {
