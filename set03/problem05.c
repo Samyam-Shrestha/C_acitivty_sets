8d5b422a7082336d3497a0bb07501729f624ebf6
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-03-01 14:03:10 +0530

    test

---
+++ set03/problem05.c
@@ -0,0 +1,25 @@
+// Write a program to find all the prime numbers between 2 to n Eratosthenes Sieve method.
+int input_array_size();
+void init_array(int n, int a[n]);
+void erotosthenes_sieve(int n, int a[n]);
+void output(int n, int a[n]);
+int main()
+{
+    
+}
+int input_array_size()
+{
+
+}
+void init_array(int n, int a[n])
+{
+    
+}
+void erotosthenes_sieve(int n, int a[n])
+{
+
+}
+void output(int n, int a[n])
+{
+
+}
---
+++ set03/problem06.c
@@ -0,0 +1,27 @@
+// Write a program to find the index of a substring of a string
+#include <stdio.h>
+void input_string(char* a, char* b);
+int sub_str_index(char* string, char* substring);
+void output(char *string, char *substring, int index);
+int main()
+{
+    char a[100] char b[100];
+    int index;
+    input_string(&a, &b);
+    
+}
+void input_string(char* a, char* b)
+{
+    printf("Enter first string: ");
+    scanf("%s", a);
+    printf("Enter second string: ");
+    scanf("%s", b);
+}
+int sub_str_index(char* string, char* substring)
+{
+
+}
+void output(char *string, char *substring, int index)
+{
+
+}
