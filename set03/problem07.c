9931f75b0231b974a2b77c5ff283818154cf1bfc
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-03-03 13:51:18 +0530

    test

---
+++ set03/problem07.c
@@ -0,0 +1,14 @@
+// Write a program to find the length of a line
+typedef struct point {
+    float x, y;
+} Point;
+
+typedef struct line {
+    Point p1, p2;
+    float distance;
+} Line;
+
+Point input_point();
+Line input_line();
+void find_length(Line *l);
+void output(Line l);
