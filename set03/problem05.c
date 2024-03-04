83ed7ff406f894a117b762f5052f298b5a34acc4
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-03-04 13:57:26 +0530

    test

--- set03/problem05.c
+++ set03/problem05.c
@@ -5,7 +5,13 @@ void erotosthenes_sieve(int n, int a[n]);
 void output(int n, int a[n]);
 int main()
 {
-    
+    int n;
+    n=input_array_size();
+    int a[n];
+    int_array(n,a);
+    erotosthenes_sieve(n,a);
+    output(n,a);
+    return 0;  
 }
 int input_array_size()
 {
