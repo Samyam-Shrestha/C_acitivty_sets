5bc93d816f9ccf1cab5786468c5325cf80e61f5a
Samyam-Shrestha <samyamstudy18@gmail.com>
2024-03-10 15:28:57 +0530

    test

--- set04/problem01.c
+++ set04/problem01.c
@@ -10,4 +10,23 @@ int main()
     add(n1,d1,n2,d2,&rn,&rd);
     output(n1,d1,n2,d2,rn,rd);
 }
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+    printf("Enter the numerator of the first number: ");
+    scanf("%d",num1);
+    printf("Enter the denominator of the first number: ");
+    scanf("%d",den1);
+    printf("Enter the numerator of the second number: ");
+    scanf("%d",num2);
+    printf("Enter the denominator of the second number: ");
+    scanf("%d",den2);
+}
+void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den)
+{
+
+}
+void output(int num1, int den1, int num2, int den2, int res_num, int res_den)
+{
+
+}
 
